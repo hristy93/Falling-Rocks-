786c5800a84f5ff60a8c5827d1ce0bb7bcdf33bc
hristy93 <hristi@gbg.bg>
2013-12-22 20:59:05 +0200

    Update
    A menu is added (exit functions are working), different shapes (6-7 of
    them) and colors (not working so well yet) of the rocks are added and
    the shape of the dwarf is different.

--- ConsoleGaming.h
+++ ConsoleGaming.h
@@ -9,46 +9,46 @@ typedef unsigned char ConsoleColor;
 class GameObject
 {
 private:
-        static const ConsoleColor DefaultColor = 0xF;
-        static ConsoleColor BackgroundColor;
-public: 
-        COORD Coordinates;
-        char Symbol;
-        ConsoleColor Color;
+	static const ConsoleColor DefaultColor = 0xF;
+	static ConsoleColor BackgroundColor;
+public:
+	COORD Coordinates;
+	char Symbol;
+	ConsoleColor Color;
 
-        GameObject::GameObject(COORD coordinates, char symbol) :
-                Coordinates(coordinates), Symbol(symbol), Color(DefaultColor)
-        {        }
+	GameObject(COORD coordinates, char symbol) :
+		Coordinates(coordinates), Symbol(symbol), Color(DefaultColor)
+	{	}
 
-        GameObject::GameObject(int x, int y, char symbol) :
-                Symbol(symbol), Color(DefaultColor)
-        {
-                COORD coordinates = { x, y };
-                this->Coordinates = coordinates;
-        }
+	GameObject(int x, int y, char symbol) :
+		Symbol(symbol), Color(DefaultColor)
+	{
+		COORD coordinates = { x, y };
+		this->Coordinates = coordinates;
+	}
 
-        GameObject::GameObject(const GameObject& gameObject) :
-                Coordinates(gameObject.Coordinates), Symbol(gameObject.Symbol), Color(DefaultColor)
-        {        }
+	GameObject(const GameObject& gameObject) :
+		Coordinates(gameObject.Coordinates), Symbol(gameObject.Symbol), Color(DefaultColor)
+	{	}
 
-        void GameObject::UpdateCoordinates(const COORD& value)
-        {
-                this->Coordinates.X = value.X;
-                this->Coordinates.Y = value.Y;
-        }
+	void UpdateCoordinates(const COORD& value)
+	{
+		this->Coordinates.X = value.X;
+		this->Coordinates.Y = value.Y;
+	}
 
-        void GameObject::Draw(HANDLE consoleOutputHandle) const
-        {
-                SetConsoleCursorPosition(consoleOutputHandle, this->Coordinates);
-                ConsoleColor color = (this->Color & 0x0F) | BackgroundColor;
-                SetConsoleTextAttribute(consoleOutputHandle, color);
-                std::cout << this->Symbol;
-        }
-        
-        static void SetBackgroundColor(ConsoleColor backgroundColor)
-        {
-                BackgroundColor = (backgroundColor & 0x0F) << 4;
-        }
+	void Draw(HANDLE consoleOutputHandle) const
+	{
+		SetConsoleCursorPosition(consoleOutputHandle, this->Coordinates);
+		ConsoleColor color = (this->Color & 0x0F) | BackgroundColor;
+		SetConsoleTextAttribute(consoleOutputHandle, color);
+		std::cout << this->Symbol;
+	}
+
+	static void SetBackgroundColor(ConsoleColor backgroundColor)
+	{
+		BackgroundColor = (backgroundColor & 0x0F) << 4;
+	}
 };
 
 void ClearScreen(HANDLE consoleHandle);
@@ -56,18 +56,18 @@ void ClearScreen(HANDLE consoleHandle);
 
 namespace ConsoleColors
 {
-        enum Colors
-        {
-                // Color space is ARGB
-                Blue = 0x1,
-                Green = 0x2,
-                Red = 0x4,
-                Yellow = Red | Green,
-                Purple = Blue | Green,
-                Cyan = Blue | Green,
-                Black = 0,
-                White = Red | Green | Blue
-        };
+	enum Colors
+	{
+		// Color space is ARGB
+		Blue = 0x1,
+		Green = 0x2,
+		Red = 0x4,
+		Yellow = Red | Green,
+		Purple = Blue | Green,
+		Cyan = Blue | Green,
+		Black = 0,
+		White = Red | Green | Blue
+	};
 }
 
 #endif
--- FallingRocks.cpp
+++ FallingRocks.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <conio.h>
 #include <time.h>
+#include <iomanip>
 
 #include "ConsoleGaming.h"
 
 using namespace std;
+using namespace ConsoleColors;
 
 HANDLE consoleHandle;
 
@@ -13,10 +15,11 @@ typedef vector<GameObject>::iterator randomAccess_iterator;
 typedef vector<GameObject>::const_iterator const_iterator;
 
 // Window constants
-const int WindowWidth = 70;
-const int WindowHeight = 30;
+const int WindowWidth = 80;
+const int WindowHeight = 20;
+
 // Rock variables
-const char RockSymbol = '#';
+char RockSymbol = '#';
 int rockSpeed = 1;
 
 // Dwarf variables
@@ -25,107 +28,265 @@ int dwarfSpeed = 1;
 // Game variables
 unsigned long sleepDuration = 200;
 
+//Collision detection
+bool Collision = false;
+
+//If the cmd will close
+bool Stay = true;
+
+//If the game is running
+bool inGame = false;
+
 vector<GameObject> dwarf;
 vector<GameObject> rocks;
 
 unsigned int frameCounter = 0;
 unsigned int rockSpawnInterval = 10;
 
+//void CheckCollision (int rockXCoord, int rockYCoord)
+//{
+//   bool Collided = false;
+//   for (randomAccess_iterator dwarfBody = dwarf.begin(); dwarfBody != dwarf.end(); ++dwarfBody)
+//   {
+//       if(dwarfBody->Coordinates.X == rockXCoord && dwarfBody->Coordinates.Y == rockYCoord)
+//       {
+//           Collided = true;
+//       }
+//   }
+//   Collision = Collided;
+//}
+
+void ShowMenu()
+{
+    ClearScreen(consoleHandle);
+	cout << " _____     _ _ _               ____            _" << endl;
+	cout << "|  ___|_ _| | (_)_ __   __ _  |  _ \\ ___   ___| | _____" << endl;
+	cout << "| |_ / _` | | | | '_ \\ / _` | | |_) / _ \\ / __| |/ / __|" << endl;
+	cout << "|  _| (_| | | | | | | | (_| | |  _ < (_) | (__|   <\\__ \\" << endl;
+	cout << "|_|  \\__,_|_|_|_|_| |_|\\__, | |_| \\_\\___/ \\___|_|\\_\\___/" << endl;
+	cout << "                       |___/" << endl;
+	int choice = 1;
+	if (!inGame)
+	{
+		cout << "Choose an option:" << endl;
+		cout << "1 - Start a new game" << endl;
+		cout << "2 - Game intructions" << endl;
+		cout << "3 - Show your highest score" << endl;
+		cout << "4 - Exit the game" << endl;
+		cout << "You can always retun to this menu by pressing the M key" << endl;
+	}
+	else
+	{
+		cout << "Choose an option:" << endl;
+		cout << "1 - Resume the game" << endl;
+		cout << "2 - Start a new game" << endl;
+		cout << "3 - Game intructions" << endl;
+		cout << "4 - Show your highest score" << endl;
+		cout << "5 - Exit the game" << endl;
+		cout << "You can always retun to this menu by pressing the M key" << endl;
+	}
+	cin >> choice;
+	switch (choice)
+	{
+		case 1:
+		{
+			return;
+		}
+		case 2:
+		{
+			if (inGame)
+			{
+		      /*
+			    rockSpeed = 1;
+				dwarfSpeed = 1;
+				sleepDuration = 200;
+				Collision = false;
+				Stay = true;
+				inGame = false;
+				frameCounter = 0;
+				rockSpawnInterval = 10;
+				goto MainFunk;
+				*/
+				break;
+			  
+			}
+		}
+		case 4:
+		{
+			if (!inGame)
+			{
+				Stay = false;
+				break;
+			}
+		}
+		case 5:
+		{
+			if (inGame)
+			{
+				Stay = false;
+				break;
+			}
+		}
+	}
+	
+}
+
 void Update()
 {
-        COORD direction = { 0, 0 };
-        if (kbhit())
-        {
-                char key = getch();
-                switch (key)
-                {
-                case 'a':
-                        direction.X = -dwarfSpeed;
-                        direction.Y = 0;
-                        break;
-                case 'w':
-                        direction.X = 0;
-                        direction.Y = -dwarfSpeed;
-                        break;
-                case 'd':
-                        direction.X = dwarfSpeed;
-                        direction.Y = 0;
-                        break;
-                case 's':
-                        direction.X = 0;
-                        direction.Y = dwarfSpeed;
-                        break;
-                };
-        }
-        for (randomAccess_iterator dwarfBody = dwarf.begin(); dwarfBody != dwarf.end(); ++dwarfBody)
-        {
-                dwarfBody->Coordinates.X += direction.X;
-                dwarfBody->Coordinates.Y += direction.Y;
-        }
+	COORD direction = { 0, 0 };
+	if (kbhit())
+	{
+		char key = getch();
+		switch (key)
+		{
+		case 'a':
+			direction.X = -dwarfSpeed;
+			direction.Y = 0;
+			break;
+		case 'w':
+			direction.X = 0;
+			direction.Y = -dwarfSpeed;
+			break;
+		case 'd':
+			direction.X = dwarfSpeed;
+			direction.Y = 0;
+			break;
+		case 's':
+			direction.X = 0;
+			direction.Y = dwarfSpeed;
+			break;
+		case 'm':
+			ShowMenu();
+			break;
+		};
+	}
+	for (randomAccess_iterator dwarfBody = dwarf.begin(); dwarfBody != dwarf.end(); ++dwarfBody)
+	{
+		dwarfBody->Coordinates.X += direction.X;
+		dwarfBody->Coordinates.Y += direction.Y;
+		//dwarfBody->Color = Green;
+	}
 
-        // Update the position of all rocks. Remove any rock that goes outside the window
+	// Update the position of all rocks. Remove any rock that goes outside the window
 
-        for (randomAccess_iterator rock = rocks.begin(); rock != rocks.end(); /* Empty clause so we can delete elements */)
-        {
-                rock->Coordinates.Y += rockSpeed;
-                if (rock->Coordinates.Y > WindowHeight - 1)
-                {
-                        rock = rocks.erase(rock);
-                }
-                else
-                {
-                        ++rock;
-                }
-        }
+	for (randomAccess_iterator rock = rocks.begin(); rock != rocks.end(); /* Empty clause so we can delete elements */)
+	{
+		rock->Coordinates.Y += rockSpeed;
+		if (rock->Coordinates.Y > WindowHeight - 1)
+		{
+			rock = rocks.erase(rock);
+		}
+		else
+		{
+			++rock;
+		}
+		// CheckCollision(rock->Coordinates.X,rock->Coordinates.Y);
+	}
 
-        if (frameCounter % rockSpawnInterval == 0)
-        {
-                // Spawn a new rock at every x frames
-                int x = rand() % WindowWidth;
-                rocks.push_back(GameObject(x, 0, RockSymbol));
-        }
-        ++frameCounter;
+	if (frameCounter % rockSpawnInterval == 0)
+	{
+		// Spawn a new rock at every x frames
+		int x = rand() % WindowWidth;
+		//		The original RockSymbol
+		//		rocks.push_back(GameObject(x, 0, RockSymbol));
+
+		rocks.push_back(GameObject(x, 0, RockSymbol));
+
+		//      Rocks in defferent shapes
+		int shapeNum = rand() % 7;
+		switch (shapeNum)
+		{
+			case 1: RockSymbol = static_cast<char>(176); break;
+			case 2: RockSymbol = static_cast<char>(219); break;
+			case 3: RockSymbol = static_cast<char>(220); break;
+			case 4: RockSymbol = static_cast<char>(222); break;
+			case 5: RockSymbol = static_cast<char>(178); break;
+			case 6: RockSymbol = static_cast<char>(254); break;
+		}
+
+		//Make stones colorful
+		randomAccess_iterator rock = rocks.end() - 1;
+		switch (shapeNum)
+		{
+		case 1: rock->Color = Red; break;
+		case 2: rock->Color = Blue; break;
+		case 3: rock->Color = White; break;
+		case 4: rock->Color = Yellow; break;
+		case 5: rock->Color = Purple; break;
+		case 6: rock->Color = Cyan; break;
+		}
+
+		//rocks.push_back(GameObject(x, 0, RockSymbol));
+	}
+	++frameCounter;
 }
 
 void Draw()
 {
-        ClearScreen(consoleHandle);
+	ClearScreen(consoleHandle);
+	//   if(!Collision)
+	for (const_iterator dwarfBody = dwarf.begin(); dwarfBody != dwarf.end(); ++dwarfBody)
+	{
+		dwarfBody->Draw(consoleHandle);
+	}
 
-        for (const_iterator dwarfBody = dwarf.cbegin(); dwarfBody != dwarf.cend(); ++dwarfBody)
-        {
-                dwarfBody->Draw(consoleHandle);
-        }
-
-        for (const_iterator rock = rocks.cbegin(); rock != rocks.cend(); ++rock)
-        {
-                rock->Draw(consoleHandle);
-        }
+	for (const_iterator rock = rocks.begin(); rock != rocks.end(); ++rock)
+	{
+		rock->Draw(consoleHandle);
+	}
 
 }
 
 int main()
 {
-        consoleHandle = GetStdHandle( STD_OUTPUT_HANDLE );
+	consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	srand(time(NULL));
+	int dwarfY = WindowHeight;
+	//  int dwarfY = WindowHeight - 1;
+	int dwarfX = WindowWidth / 2;
 
-        srand(time(NULL));
+	char dwarfSymbol = '*';
 
-        int dwarfY = WindowHeight - 1;
-        int dwarfX = WindowWidth / 2;
-        char dwarfSymbol = '*';
+	//  Dwarf shape 1
+	//  dwarf.push_back(GameObject(dwarfX - 1, dwarfY, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX + 1, dwarfY, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX, dwarfY - 1, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX, dwarfY - 2, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX - 1, dwarfY - 2, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX + 1, dwarfY - 2, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX, dwarfY - 3, dwarfSymbol));
 
-        dwarf.push_back(GameObject(dwarfX - 1, dwarfY - 1, dwarfSymbol));
-        dwarf.push_back(GameObject(dwarfX + 1, dwarfY - 1, dwarfSymbol));
-        dwarf.push_back(GameObject(dwarfX, dwarfY, dwarfSymbol));
-        dwarf.push_back(GameObject(dwarfX - 1, dwarfY + 1, dwarfSymbol));
-        dwarf.push_back(GameObject(dwarfX + 1, dwarfY + 1, dwarfSymbol));
-        
+	//  Dwarf shape 2
+	char dwarfSymbol1 = '#';
+	char dwarfSymbol2 = '^';
+	dwarf.push_back(GameObject(dwarfX - 1, dwarfY, dwarfSymbol2));
+	dwarf.push_back(GameObject(dwarfX + 1, dwarfY, dwarfSymbol2));
+	dwarf.push_back(GameObject(dwarfX, dwarfY - 1, dwarfSymbol1));
+	dwarf.push_back(GameObject(dwarfX, dwarfY - 2, dwarfSymbol1));
+	dwarf.push_back(GameObject(dwarfX - 1, dwarfY - 2, dwarfSymbol));
+	dwarf.push_back(GameObject(dwarfX + 1, dwarfY - 2, dwarfSymbol));
+	dwarf.push_back(GameObject(dwarfX, dwarfY - 3, dwarfSymbol));
 
-        while (true)
-        {
-                Update();
-                Draw();
-                Sleep(sleepDuration);
-        }
+	//  The original shape ot the dwarf
+	//	dwarf.push_back(GameObject(dwarfX - 1, dwarfY - 1, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX + 1, dwarfY - 1, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX, dwarfY, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX - 1, dwarfY + 1, dwarfSymbol));
+	//	dwarf.push_back(GameObject(dwarfX + 1, dwarfY + 1, dwarfSymbol));
 
-        return 0;
+	ShowMenu();
+	inGame = true;
+	while (Stay)
+	{
+		//Stay = Update();
+	    Update();
+		if (Stay == false)
+		{
+			break;
+		}
+		Draw();
+		Sleep(sleepDuration);
+	}
+
+	return 0;
 }
