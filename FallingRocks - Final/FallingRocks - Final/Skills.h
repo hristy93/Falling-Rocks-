c3b6214a68595b4e7f39b88b8a6df3334f6549d4
PreskoIsTheGreatest <preskosemov@abv.bg>
2014-01-21 01:37:45 +0200

    Delete Skills.h

--- FallingRocks - Final/FallingRocks - Final/Skills.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void OutputOfAndromedaSkill();
-void OutputOfFireSkill();
-void OutputOfPhaseShiftSkill();
-void AndromedaSkill();
-void FireDuration();
-void SkillDuration();
-void OnFireClick();
