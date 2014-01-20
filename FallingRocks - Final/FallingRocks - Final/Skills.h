ba33d4f4b58fb3d7bc052beeaa4c71249a696f9a
PreskoIsTheGreatest <preskosemov@abv.bg>
2014-01-21 00:29:40 +0200

    Create Skills.h

---
+++ FallingRocks - Final/FallingRocks - Final/Skills.h
@@ -0,0 +1,8 @@
+#pragma once
+void OutputOfAndromedaSkill();
+void OutputOfFireSkill();
+void OutputOfPhaseShiftSkill();
+void AndromedaSkill();
+void FireDuration();
+void SkillDuration();
+void OnFireClick();
